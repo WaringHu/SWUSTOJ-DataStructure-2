043636c9384137b2a487672c8d26f0bdd08b6eac
WaringHu <waringhu@163.com>
2019-05-15 23:56:32 +0800

    index

---
+++ Codes/#1055 邻接矩阵到邻接表/index.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[100][100];
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> arr[i][j];
+		}
+	}
+	int m, k;
+	for (m = 0; m < n; m++)
+	{
+		for (k = 0; k < n; k++)
+		{
+			if (arr[m][k] == 1)
+			{
+				cout << k;
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
